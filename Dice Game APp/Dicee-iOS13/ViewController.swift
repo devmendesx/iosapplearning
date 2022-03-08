4e5d0cef102641966f334ca7751cf30ebb588b61
math <matheushenriquesad@gmail.com>
2022-03-08 16:23:42 -0300

    Dice Game Added

---
+++ Dice Game APp/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceImageViewOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")].randomElement()
+        diceImageViewTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")].randomElement()
+    }
+}
+
